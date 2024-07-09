a5764da55f7706ee64eb6b1f4d080dcd0c58e878
Giovanni Ravalico <15946771+suddenlyGiovanni@users.noreply.github.com>
2024-07-09 22:55:04 +0200

    wip

--- src/router.test.ts
+++ src/router.test.ts
@@ -1,45 +1,64 @@
 import assert from 'node:assert/strict'
-import { describe, it } from 'node:test'
-import Router from './router'
+import { describe, test } from 'node:test'
+// import Router from './router'
 
-describe('Router', () => {
-	describe('restore', () => {
-		it('should restore properties after function execution', () => {
-			// biome-ignore lint/complexity/noStaticOnlyClass: <explanation>
-			class TestRouter extends Router {
-				public static override restore<
-					Obj extends object,
-					ObjKeysToRestore extends Array<keyof Obj>,
-					Fn extends Function,
-				>(fn: Fn, obj: Obj, ...objKeysToRestore: ObjKeysToRestore) {
-					return Router.restore(fn, obj, ...objKeysToRestore)
-				}
+describe('restore', () => {
+	/**
+	 * Restore obj props after function
+	 *
+	 * @private
+	 */
+	function restore(fn, obj) {
+		var props = new Array(arguments.length - 2)
+		var vals = new Array(arguments.length - 2)
+
+		for (var i = 0; i < props.length; i++) {
+			props[i] = arguments[i + 2]
+			vals[i] = obj[props[i]]
+		}
+
+		return function () {
+			// restore vals
+			for (var i = 0; i < props.length; i++) {
+				obj[props[i]] = vals[i]
 			}
 
-			const testFunction = function (this: typeof testObject) {
-				this.prop1 = 'temp'
-				this.prop2 = 'temp'
-			}
+			return fn.apply(this, arguments)
+		}
+	}
 
-			const testObject = {
-				prop1: 'original',
-				prop2: 'original',
-			}
+	test('restore function', () => {
+		// Initial object
+		const obj = { a: 1, b: 2 }
 
-			// Call the restore method
-			const restoredFunction = TestRouter.restore(
-				testFunction.bind(testObject),
-				testObject,
-				'prop1',
-				'prop2',
-			)
+		// Assert initial object properties
+		assert.strictEqual(obj.a, 1, 'Initial value of a should be 1')
+		assert.strictEqual(obj.b, 2, 'Initial value of b should be 2')
 
-			// Execute the returned function
-			restoredFunction()
+		// Function to be passed to restore (does nothing in this test)
+		const fn = () => {}
 
-			// Check the values
-			assert.equal(testObject.prop1, 'original')
-			assert.equal(testObject.prop2, 'original')
-		})
+		// Modify object properties
+		obj.a = 3
+		obj.b = 4
+
+		// Assert modified object properties
+		assert.strictEqual(obj.a, 3, 'Value of a after modification should be 3')
+		assert.strictEqual(obj.b, 4, 'Value of b after modification should be 4')
+
+		// Call restore to save current state and get a restore function
+		const restoreFn = restore(fn, obj, 'a', 'b')
+
+		// Modify properties again
+		obj.a = 5
+		obj.b = 6
+
+		// Use the restore function to restore original properties
+		restoreFn()
+
+		// Assert properties are restored to their original values
+		// Assert properties are restored to their values at the time of the restore function call
+		assert.strictEqual(obj.a, 3, 'Value of a after restore should be 3')
+		assert.strictEqual(obj.b, 4, 'Value of b after restore should be 4')
 	})
 })
